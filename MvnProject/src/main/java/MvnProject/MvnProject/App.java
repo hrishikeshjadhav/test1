0a98ba8ff971cf862b2ec48c716c5c4948aea1e5
hrishikeshjadhav <hrishi.jadhav.study@gmail.com>
2017-04-04 22:53:42 +0530

    Uploading test mvn project
    mvn project for github

---
+++ MvnProject/src/main/java/MvnProject/MvnProject/App.java
@@ -0,0 +1,14 @@
+package MvnProject.MvnProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main( String[] args )
+	{
+		System.out.println( "Hello World!" );
+
+	}
+}
